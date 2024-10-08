94012f953de0d0fd5f0fb0af1eca75c66f991be5
Guilherme <gui.social.ti@gmail.com>
2024-10-08 16:28:07 -0300

    Arrays PT.1

---
+++ arrays/arraysPT1.java
@@ -0,0 +1,15 @@
+public class arraysPT1 {
+    public static void main(String[] args) {
+        // type[] name   
+        // initialization with null
+        int[] idades = null;
+        System.out.println(idades);
+        
+        // Initialization as object
+        int[] nomes = new int[3]; // 3 = memory allocation
+        nomes[0] = 21;
+        System.out.println(nomes[0]); // Print the name in position 0
+
+
+    }
+}
